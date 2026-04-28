e6936be4a123901de18aba55d095deb346f1d073
unknown <jaballahs@SIOPERGAUD.FR>
2026-04-28 16:55:37 +0200

    ajout de finidutilisateurbypseudo

--- projet-cinesio-tp1/projet-cinesio-tp1/src/repositories/utilisateurRepository.php
+++ projet-cinesio-tp1/projet-cinesio-tp1/src/repositories/utilisateurRepository.php
@@ -14,3 +14,19 @@ function findUtilisateurByEmail(string $email): array
     $resultat = $requete->fetch(PDO::FETCH_ASSOC);
     return $resultat;
 }
+
+//findUtilisateurByPseudo(string $pseudo) : pour vérifier si un pseudo est déjà utilisé.
+function findUtilisateurByPseudo(string $pseudo): array
+{
+    require_once __DIR__ . '/../database/connection.php';
+    $connexion = connectDatabase();
+
+    $requeteSql = "SELECT id, nom, prenom, email, mot_de_passe FROM utilisateur WHERE pseudo = :pseudo";
+
+    $requete = $connexion->prepare($requeteSql);
+    $requete->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
+    $requete->execute();
+
+    $resultat = $requete->fetch(PDO::FETCH_ASSOC);
+    return $resultat;
+}
