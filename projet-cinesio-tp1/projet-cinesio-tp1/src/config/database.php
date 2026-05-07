b24c3725b7ef7d2234229358c261e8be9923758f
salimjaballah1-oss <salim.jaballah1@gmail.com>
2026-05-07 09:27:48 +0200

    modif css

--- projet-cinesio-tp1/projet-cinesio-tp1/src/config/database.php
+++ projet-cinesio-tp1/projet-cinesio-tp1/src/config/database.php
@@ -1,6 +1,6 @@
 <?php
 const DB_HOST = 'aws-1-eu-west-3.pooler.supabase.com';
-const DB_PORT = 5432;    //facultatif
+const DB_PORT = 5432;    
 const DB_NAME = 'postgres';
 const DB_USER = 'postgres.lxgbudtdyxlngvkqpqvh';
 const DB_PASSWORD = 'Eyaamine394545!';
